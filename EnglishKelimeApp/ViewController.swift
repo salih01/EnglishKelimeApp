1dc8a11d4eb0c9430108120a3490017a2200d158
Salih Çakmak <slhckmk01@gmail.com>
2023-07-05 22:30:02 +0300

    Initial Commit

---
+++ EnglishKelimeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnglishKelimeApp
+//
+//  Created by BilmSoft on 5.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
