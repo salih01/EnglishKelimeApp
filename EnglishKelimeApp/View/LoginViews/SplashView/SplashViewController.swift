da2fdc212e04aaf3a63264798a173b93174e42e5
Salih Çakmak <slhckmk01@gmail.com>
2023-07-10 22:40:09 +0300

    Login bug Fixed

--- EnglishKelimeApp/View/LoginViews/SplashView/SplashViewController.swift
+++ EnglishKelimeApp/View/LoginViews/SplashView/SplashViewController.swift
@@ -8,41 +8,41 @@
 import UIKit
 
 class SplashViewController: UIViewController {
-
+    
     var isUserLoggedIn:Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         backGround()
     }
-    override func viewDidAppear(_ animated: Bool) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.showinitialView()
-
+    private func showinitialView() {
+        if isUserLoggedIn {
+            let vc = MainViewController()
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true, completion: nil)
+        } else {
+            let vc = OnboardingViewController()
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true, completion: nil)
         }
     }
     
-
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.showinitialView()
+        }
+    }
+    
+    
     func backGround(){
         
         view.backgroundColor = .systemGreen
     }
     
-    private func showinitialView(){
-        
-        if isUserLoggedIn {
-            
-            let vc :MainViewController = MainViewController()
-            vc.modalPresentationStyle = .fullScreen
-            vc.present(vc, animated: true)
-            
-        } else {
-            let vc:OnboardingViewController = OnboardingViewController()
-            vc.modalPresentationStyle = .fullScreen
-            present(vc, animated: true)
-        }
-        
-    }
     
     
 }
