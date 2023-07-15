e4525fb03a6dfcce26540387039fbe9efacd5c95
Salih Çakmak <slhckmk0@gmail.com>
2023-07-15 14:42:01 +0300

    Fixed git Account

--- EnglishKelimeApp/Extensions/UIViewExtensions.swift
+++ EnglishKelimeApp/Extensions/UIViewExtensions.swift
@@ -44,25 +44,30 @@ extension UIButton {
         layer.insertSublayer(shadowLayer, at: 0)
     }
 }
-extension UIButton {
-    /// Adds a shadow to the button, with a corner radius
-    /// - Parameters:
-    ///   - corner: The corner radius to apply to the shadow and button
-    ///   - color: The color of the shaodw
-    ///   - opacity: The opacity of the shadow
-    ///   - offset: The offset of the shadow
-    ///   - radius: The radius of the shadow
-    func addShadows(corner: CGFloat = 20, color: UIColor = .black, opacity: Float = 0.3, offset: CGSize = CGSize(width: 0, height: 5), radius: CGFloat = 5) {
-        let shadowLayer = CAShapeLayer()
-        layer.cornerRadius = corner
-        shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: corner).cgPath
-        shadowLayer.fillColor = UIColor.clear.cgColor
-        shadowLayer.shadowColor = color.cgColor
-        shadowLayer.shadowPath = shadowLayer.path
-        shadowLayer.shadowOffset = offset
-        shadowLayer.shadowOpacity = opacity
-        shadowLayer.shadowRadius = radius
-        
-        layer.insertSublayer(shadowLayer, at: 0)
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
     }
+
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+}
+extension UILabel {
+    func addIcon(icon: UIImage, text: String, iconSize: CGSize, xOffset: CGFloat, yOffset: CGFloat) {
+           let attachment = NSTextAttachment()
+           attachment.image = icon
+           attachment.bounds = CGRect(origin: CGPoint(x: xOffset, y: yOffset), size: iconSize)
+
+           let attachmentString = NSAttributedString(attachment: attachment)
+           let mutableAttributedString = NSMutableAttributedString(string: "")
+           mutableAttributedString.append(attachmentString)
+
+           let textString = NSMutableAttributedString(string: " \(text)")
+           mutableAttributedString.append(textString)
+
+           self.attributedText = mutableAttributedString
+       }
 }
