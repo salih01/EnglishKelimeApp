3d7a3d4aa88f24ef35cdcc8b597c6cac55798bbc
Salih Çakmak <slhckmk0@gmail.com>
2023-07-22 22:03:34 +0300

    Onboarding View image

--- EnglishKelimeApp/Model/Onboarding/SlideOnboarding.swift
+++ EnglishKelimeApp/Model/Onboarding/SlideOnboarding.swift
@@ -14,10 +14,20 @@ struct Slide {
     
     static let collection:[Slide] = [
     
-        Slide(imageName: "onboarding1", title: "onboarding1", description: "onboarding1onboarding1"),
-        Slide(imageName: "onboarding1", title: "onboarding2", description: "onboarding1onboarding2222"),
-        Slide(imageName: "onboarding1", title: "onboarding3", description: "onboarding1onboarding3333"),
-        Slide(imageName: "onboarding1", title: "onboarding4", description: "onboarding1onboarding4444")
+        Slide(imageName: "cikcik1", title: "onboarding1", description: "onboarding1onboarding1"),
+        Slide(imageName: "cikcik2", title: "onboarding2", description: "onboarding1onboarding2222"),
+        Slide(imageName: "cikcik3", title: "onboarding3", description: "onboarding1onboarding3333"),
+        Slide(imageName: "cikick4", title: "onboarding4", description: "onboarding1onboarding4444"),
+        Slide(imageName: "cikick5", title: "onboarding4", description: "onboarding1onboarding4444"),
+        Slide(imageName: "cikick6", title: "onboarding4", description: "onboarding1onboarding4444"),
+        Slide(imageName: "cikick7", title: "onboarding4", description: "onboarding1onboarding4444"),
+        Slide(imageName: "cikick8", title: "onboarding4", description: "onboarding1onboarding4444"),
+        Slide(imageName: "cikick9", title: "onboarding4", description: "onboarding1onboarding4444"),
+
+
+
+        
+
 
     ]
 }
