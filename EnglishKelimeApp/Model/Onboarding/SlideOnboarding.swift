2736be9a29873fbfa60bc4ddd702cbf8f1de6eeb
Salih Çakmak <slhckmk0@gmail.com>
2023-07-23 23:43:11 +0300

    New images Onboarding

--- EnglishKelimeApp/Model/Onboarding/SlideOnboarding.swift
+++ EnglishKelimeApp/Model/Onboarding/SlideOnboarding.swift
@@ -14,20 +14,15 @@ struct Slide {
     
     static let collection:[Slide] = [
     
-        Slide(imageName: "cikcik1", title: "onboarding1", description: "onboarding1onboarding1"),
-        Slide(imageName: "cikcik2", title: "onboarding2", description: "onboarding1onboarding2222"),
-        Slide(imageName: "cikcik3", title: "onboarding3", description: "onboarding1onboarding3333"),
-        Slide(imageName: "cikick4", title: "onboarding4", description: "onboarding1onboarding4444"),
-        Slide(imageName: "cikick5", title: "onboarding4", description: "onboarding1onboarding4444"),
-        Slide(imageName: "cikick6", title: "onboarding4", description: "onboarding1onboarding4444"),
-        Slide(imageName: "cikick7", title: "onboarding4", description: "onboarding1onboarding4444"),
-        Slide(imageName: "cikick8", title: "onboarding4", description: "onboarding1onboarding4444"),
-        Slide(imageName: "cikick9", title: "onboarding4", description: "onboarding1onboarding4444"),
-
-
-
-        
-
+        Slide(imageName: "1", title: "onboarding1", description: "onboarding1onboarding1"),
+        Slide(imageName: "2", title: "onboarding2", description: "onboarding1onboarding2222"),
+        Slide(imageName: "3", title: "onboarding3", description: "onboarding1onboarding3333"),
+        Slide(imageName: "4", title: "onboarding4", description: "onboarding1onboarding4444"),
+        Slide(imageName: "5", title: "onboarding4", description: "onboarding1onboarding4444"),
+        Slide(imageName: "6", title: "onboarding4", description: "onboarding1onboarding4444"),
+        Slide(imageName: "7", title: "onboarding4", description: "onboarding1onboarding4444"),
+        Slide(imageName: "8", title: "onboarding4", description: "onboarding1onboarding4444"),
+        Slide(imageName: "9", title: "onboarding4", description: "onboarding1onboarding4444")
 
     ]
 }
