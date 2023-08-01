8a31088093c7f6d0895cc92d9216c2850cdfbda4
Salih Çakmak <slhckmk0@gmail.com>
2023-08-01 22:59:35 +0300

    SignUp View Created

---
+++ EnglishKelimeApp/View/LoginViews/SignUpView/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  EnglishKelimeApp
+//
+//  Created by BilmSoft on 1.08.2023.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
