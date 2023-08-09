2db735f90a2915e274594fcb0a745317df13a8ae
Salih Çakmak <slhckmk0@gmail.com>
2023-08-09 20:58:02 +0300

    Update SlideOnboarding.swift

--- EnglishKelimeApp/Model/Onboarding/SlideOnboarding.swift
+++ EnglishKelimeApp/Model/Onboarding/SlideOnboarding.swift
@@ -15,7 +15,7 @@ struct Slide {
     static let collection:[Slide] = [
     
         Slide(imageName: "6", title: "İngilizce Kelime Öğrenmek Şimdi Çok daha kolay", description: "Tek yapmanız gereken testi çözmek ve cevapları doğru cevaplamak "),
-        Slide(imageName: "8", title: "onboarding2", description: "Tek yapmanız gereken testi çözmek ve cevapları doğru cevaplamak "),
+        Slide(imageName: "8", title: "onboarding2", description: "Tek gereken testi çözmek ve cevapları doğru cevaplamak "),
         Slide(imageName: "2", title: "onboarding3", description: "Tek yapmanız gereken testi çözmek ve cevapları doğru cevaplamak "),
         Slide(imageName: "3", title: "onboarding4", description: "Tek yapmanız gereken testi çözmek ve cevapları doğru cevaplamak ")
         
