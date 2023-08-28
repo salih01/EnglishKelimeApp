9f2da88464de2b3f7c1e7b2d1890357c92b80776
Salih Cakmak <slhckmk0@gmail.com>
2023-08-28 22:50:57 +0300

    Settings UI Update

--- EnglishKelimeApp/View/MainView/SettingsView/SettingsViewController.swift
+++ EnglishKelimeApp/View/MainView/SettingsView/SettingsViewController.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
+
+    @IBOutlet weak var loginView: UIVisualEffectView!
+    @IBOutlet weak var notificationsView: UIVisualEffectView!
+    @IBOutlet weak var memberView: UIVisualEffectView!
+    @IBOutlet weak var abautUsView: UIVisualEffectView!
+    @IBOutlet weak var privacyView: UIVisualEffectView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
         
+    }
+    
+    func setupView(){
+        loginView.layer.cornerRadius = 10
+        loginView.clipsToBounds = true
+        
+        notificationsView.layer.cornerRadius = 10
+        notificationsView.clipsToBounds = true
+        
+        memberView.layer.cornerRadius = 10
+        memberView.clipsToBounds = true
+        
+        abautUsView.layer.cornerRadius = 10
+        abautUsView.clipsToBounds = true
+        
+        privacyView.layer.cornerRadius = 10
+        privacyView.clipsToBounds = true
     }
 
 
