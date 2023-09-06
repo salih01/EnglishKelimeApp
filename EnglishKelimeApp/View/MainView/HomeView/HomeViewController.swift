d34fa6a31e595db8467998836e9c7d178276c5dc
Salih Cakmak <slhckmk0@gmail.com>
2023-09-06 23:14:01 +0300

    HomeView started

--- EnglishKelimeApp/View/MainView/HomeView/HomeViewController.swift
+++ EnglishKelimeApp/View/MainView/HomeView/HomeViewController.swift
@@ -9,24 +9,38 @@ import UIKit
 import Lottie
 
 class HomeViewController: UIViewController {
-
+    
     @IBOutlet weak var animationView: LottieAnimationView!
+    
+    private var isDarkModeEnabled = false // Kullanılacak arayüz stilini kontrol etmek için bir bayrak
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAnimation()
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Belirli bir süre sonra karanlık moda geçmek için bir zamanlayıcı başlat
+        Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(switchToDarkMode), userInfo: nil, repeats: false)
+    }
+    
     private func setupAnimation() {
-        animationView.animation = LottieAnimation.named("wawes")
+        let animationName = isDarkModeEnabled ? "back&black" : "white&whites"
+        
+        animationView.animation = LottieAnimation.named(animationName)
         animationView.contentMode = .scaleAspectFill
-           animationView.loopMode = .loop
-           animationView.animationSpeed = 1
-           animationView.play()
+        animationView.loopMode = .loop
+        animationView.animationSpeed = 1
+        animationView.play()
+    }
+    
+    @objc private func switchToDarkMode() {
+        isDarkModeEnabled = true
+        setupAnimation()
         
-        
-        
-       }
-
+        // Burada ayrıca arayüzünüzü karanlık moda geçirmek için gerekli diğer işlemleri yapabilirsiniz.
+    }
+    
+    
 }
